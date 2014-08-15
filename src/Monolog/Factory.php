5e8d261317d4ae7b051a7177bcfa5cbcb0adf582
Pavel Kučera <pave.kucera@gmail.com>
2014-08-15 18:09:14 +0200

    Class factory.

---
+++ src/Monolog/Factory.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Copyright (c) 2014 Pavel Kučera (http://github.com/pavelkucera)
+ */
+
+namespace Kucera\Monolog;
+
+use Monolog\Logger;
+
+
+class Factory
+{
+    /**
+     * @param array $info
+     * @return \Tracy\BlueScreen
+     */
+    public static function blueScreen(array $info = array())
+    {
+        $blueScreen = new \Tracy\BlueScreen();
+        $blueScreen->info = array_merge(array(
+            'PHP ' . PHP_VERSION,
+            isset($_SERVER['SERVER_SOFTWARE']) ? $_SERVER['SERVER_SOFTWARE'] : NULL,
+            'Tracy ' . \Tracy\Debugger::$version,
+        ), $info);
+        return $blueScreen;
+    }
+
+
+    /**
+     * @param string $logDirectory
+     * @param int $level
+     * @param bool $bubble
+     * @param \Tracy\BlueScreen $blueScreen
+     * @return Handler\BlueScreenHandler
+     */
+    public static function blueScreenHandler($logDirectory, $level = Logger::DEBUG, $bubble = TRUE, \Tracy\BlueScreen $blueScreen = NULL)
+    {
+        $blueScreen = $blueScreen ?: static::blueScreen();
+        return new Handler\BlueScreenHandler($blueScreen, $logDirectory, $level, $bubble);
+    }
+}
---
+++ tests/Monolog/FactoryTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Kucera\Monolog\Tests;
+
+use Kucera\Monolog\Factory;
+
+
+class BlueScreenFactoryTest extends TestCase
+{
+    public function testBlueScreen()
+    {
+        $blueScreen = Factory::blueScreen();
+        $this->assertInstanceOf('Tracy\BlueScreen', $blueScreen);
+    }
+
+
+    public function testBlueScreenHandler()
+    {
+        $handler = Factory::blueScreenHandler(__DIR__);
+
+    }
+}
